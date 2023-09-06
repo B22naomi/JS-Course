a7cc929a143cf4e2aa5e997717aae79f865f754c
Naomi Bitutu <naomibitutu@gmail.com>
2023-09-06 15:46:20 +0300

    continuation of the project

--- todo-list.js
+++ todo-list.js
@@ -34,6 +34,10 @@ function renderTodoList(){
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+
+  const dueDate = dateInputElement.value;
   
   todoList.push(name);
 
